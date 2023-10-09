75bb910473c340eea940c79252ee364c70c218a2
capricorn <8250820+capricorn@users.noreply.github.com>
2023-10-08 18:20:21 -0600

    Add min ios version to package spec

--- Package.swift
+++ Package.swift
@@ -5,6 +5,10 @@ import PackageDescription
 
 let package = Package(
     name: "ssregex",
+    platforms: [
+        //.macOS(.v10_13),
+        .iOS(.v16)
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
