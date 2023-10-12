f4dd447f54012d46a8106ee460a2ab26fe6415bd
capricorn <8250820+capricorn@users.noreply.github.com>
2023-10-12 14:56:42 -0600

    Add macos to package build

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "ssregex",
     platforms: [
-        //.macOS(.v10_13),
+        .macOS(.v10_13),
         .iOS(.v16)
     ],
     products: [
