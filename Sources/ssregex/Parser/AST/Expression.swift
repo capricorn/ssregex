e9407405c4c27fab6825a781022f8926d3154655
capricorn <8250820+capricorn@users.noreply.github.com>
2023-10-13 11:47:12 -0600

    Move Rewrite to separate file

--- Sources/ssregex/Parser/AST/Expression.swift
+++ Sources/ssregex/Parser/AST/Expression.swift
@@ -14,51 +14,6 @@ indirect enum Expression: CustomStringConvertible {
     case union(left: Expression, right: Expression)
     case concat([Expression])
     
-    enum Rewrite {
-        case removeExtraneousConcat
-        case collapseQuantifiers
-        case reducePlusQuantifier
-        
-        static func reducePlusQuantifier(_ expr: Expression) -> Expression {
-            if case .quantifier(let quantifier, let subexpr) = expr, quantifier == .oneOrMore {
-                return .concat([subexpr, .quantifier(operator: .zeroOrMore, subexpr)])
-            }
-            
-            return expr
-        }
-        
-        static func removeExtraneousConcat(_ expr: Expression) -> Expression {
-            switch expr {
-            case .concat(let array):
-                if array.count == 1 {
-                    return array[0]
-                }
-            default: 
-                break
-            }
-            
-            return expr
-        }
-        
-        static func collapseQuantifiers(_ expr: Expression) -> Expression {
-            switch expr {
-            case .quantifier(let quantifier, let subexpr):
-                switch subexpr {
-                case .quantifier(let subquantifier, _):
-                    if quantifier == subquantifier {
-                        return subexpr
-                    }
-                default:
-                    break
-                }
-            default:
-                break
-            }
-            
-            return expr
-        }
-    }
-    
     // TODO
     // Expression should be in a reduced form that's amenable to testing.
     var partial: Expression {
@@ -103,43 +58,7 @@ indirect enum Expression: CustomStringConvertible {
         return self
     }
     
-    // Can do polymorphism to have enum cases of defined transforms, use dot notation to advantage.
-    func rewrite(_ rewriter: (Expression) -> Expression) -> Expression {
-        return Expression.rewrite(self, rewriter: rewriter)
-    }
-    
-    func rewrite(_ rewriter: Rewrite) -> Expression {
-        let transform: (Expression) -> Expression = switch rewriter {
-        case .removeExtraneousConcat:
-            Expression.Rewrite.removeExtraneousConcat
-        case .collapseQuantifiers:
-            Expression.Rewrite.collapseQuantifiers
-        case .reducePlusQuantifier:
-            Expression.Rewrite.reducePlusQuantifier
-        }
-        
-        return rewrite(transform)
-    }
-    
-    static func rewrite(_ expr: Expression, rewriter: (Expression) -> Expression) -> Expression {
-        // Idea is: rewrite will return the transformed version of the node
-        // Problem is: if rewriter is a no-op, then what
-        let transformedExpr = rewriter(expr)
-        
-        // Take the rewritten version, go from there
-        switch transformedExpr {
-        case .quantifier(let quantifier, let expression):
-            return .quantifier(operator: quantifier, rewrite(expression, rewriter: rewriter))
-        case .union(let left, let right):
-            return .union(left: rewrite(left, rewriter: rewriter), right: rewrite(right, rewriter: rewriter))
-        case .concat(let array):
-            return .concat(array.map({rewrite($0, rewriter: rewriter)}))
-        default:
-            break
-        }
-        
-        return transformedExpr
-    }
+
     
     private static func parseTree(_ tree: Parser.Paren) -> Expression {
         var expressions: [Expression] = []
